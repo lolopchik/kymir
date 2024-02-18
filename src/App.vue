7df0f8ecce4c7622321f140f9d186e434873fd77
lolopchik <147331169+lolopchik@users.noreply.github.com>
2024-02-18 20:05:39 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import PasswordInput from './components/SpyGameTemplate.vue'
+export default {
+  components: {
+    PasswordInput,
+  }
+}
+</script>
+<template>
+
+<PasswordInput/>
+</template>
+
+<style>
+</style>
